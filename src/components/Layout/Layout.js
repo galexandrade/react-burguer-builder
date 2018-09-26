1d40de7c3ce4300417764ea257327152ba9c265d
Alex Andrade <g.alex.andrade@gmail.com>
2018-09-26 08:39:49 -0300

    Layout structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Wrapper from '../../hoc/Wrapper';
+
+const layout = (props) => (
+    <Wrapper>
+        <div>
+            Toolbar
+            SideDrawer,
+            Backdrop
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </Wrapper>
+);
+
+export default layout;
---
+++ src/hoc/Wrapper.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const wrapper = (props) => props.children;
+
+export default wrapper;
